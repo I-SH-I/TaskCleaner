cde24a29cd1bc26cf63c2a50fe9de5e4c638a14b
antyoku <antyokunaoyasu@gmail.com>
2024-05-31 14:33:42 +0900

    vercel settings

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Flaskインスタンス作成。__name__は、このモジュール名の参照
 app = Flask(__name__)
@@ -12,6 +15,23 @@ api = Api(app)
 # データベース設定
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+# 環境変数からユーザーとパスワードを取得
+basic_auth_username = os.environ.get('BASIC_AUTH_ID')
+basic_auth_password = os.environ.get('BASIC_AUTH_PASS')
+
+if not basic_auth_username or not basic_auth_password:
+    raise ValueError("BASIC_AUTH_ID and BASIC_AUTH_PASS must be set in the environment variables")
+
+users = {
+    basic_auth_username: generate_password_hash(basic_auth_password)
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
 
 # データベースモデルを定義
 class Task(db.Model):
@@ -25,18 +45,20 @@ with app.app_context():
 
 # タスクをデータベースに追加するためのメソッドを持つ
 class TaskResource(Resource):
-    # GET タスクを取得
+    @auth.login_required
     def get(self):
         tasks = Task.query.all()
         return [{'id': task.id, 'title': task.title, 'description': task.description} for task in tasks], 200
-    # POST タスクを追加
+
+    @auth.login_required
     def post(self):
         data = request.get_json()  # クライアントから送信されたJSONデータを取得
         new_task = Task(title=data['title'], description=data['description'])  # 新しいタスクを作成
         db.session.add(new_task)  # 新しいタスクをデータベースセッションに追加
         db.session.commit()  # データベースにコミット
         return {'id': new_task.id, 'title': new_task.title, 'description': new_task.description}, 201  # 新しく作成されたタスクを返す
-    # PUT タスクを更新
+
+    @auth.login_required
     def put(self):
         data = request.get_json()
         task = Task.query.get(data['id'])
@@ -46,7 +68,8 @@ class TaskResource(Resource):
         task.description = data['description']
         db.session.commit()
         return {'id': task.id, 'title': task.title, 'description': task.description}, 200
-    # DELETE タスクを削除
+
+    @auth.login_required
     def delete(self):
         data = request.get_json()
         task = Task.query.get(data['id'])
@@ -58,7 +81,6 @@ class TaskResource(Resource):
 
 # リソースクラスをエンドポイントにマッピングする。このエンドポイントに対するHTTPリクエストがTaskResourceクラスのメソッドで処理される
 api.add_resource(TaskResource, '/tasks')
-
 
 # Flaskアプリケーション実行
 if __name__ == '__main__':
